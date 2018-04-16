1f0df5d2a6bacd97fe4145570f56762bf696213f
Sam Grinis <samgrinis@Sams-MacBook-Pro.local>
2018-04-16 19:37:31 -0400

    install prop-types as separate package

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const TodoItem = (props) => {
   return (
@@ -9,7 +10,7 @@ export const TodoItem = (props) => {
 }
 
 TodoItem.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  isComplete: React.PropTypes.bool,
-  id: React.PropTypes.number.isRequired
+  name: PropTypes.string.isRequired,
+  isComplete: PropTypes.bool,
+  id: PropTypes.number.isRequired
 }

be2d55ab77392db36865fa0a1ec75ccc33913a87
Sam Grinis <samgrinis@Sams-MacBook-Pro.local>
2018-04-16 21:08:30 -0400

    install prop-types as a seperate package and add window. prior to history.pushstate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo'
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   state = {
@@ -16,7 +17,7 @@ class App extends Component {
   }
 
   static contextTypes = {
-    route: React.PropTypes.string
+    route: PropTypes.string
   }
 
   handleRemove = (id, evt) => {
